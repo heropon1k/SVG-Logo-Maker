b26efbec2dc808b513c4d8a02783af3819fa9e5e
unknown <brian.zhen101@gmail.com>
2024-07-29 01:22:02 -0400

    comments

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const inquirer = require('inquirer');
 const colors = require('colors');
 const fs = require('fs');
+//Input limit include
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+//include shapes.js
 const render = require("./lib/shapes.js")
 
+//register input limit
 inquirer.registerPrompt('maxlenght-input', MaxLengthInputPrompt)
         
+//Render SVG file given input
 function renderSVG(input){
+    //Calls generate from shapes.js
     let svg = render.generate(input)
     console.log(svg);
 
+    //write to logo.svg
     fs.writeFile("logo.svg", svg,(err =>
         err ? console.error(err) : console.log("Generated logo.svg")
     ))
